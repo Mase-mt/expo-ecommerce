917865e77481db1b8e53a47decfec46b68f89359
Mase-mt <masamomathewos9@gmail.com>
2025-12-10 23:26:02 +0300

    fixed admin routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,10 +12,10 @@ app.get("/api/health", (req,res) => {
 
 //make our app ready for deployment
 if(ENV.NODE_ENV === "development"){
-    app.use(express.static(path.join(__dirname, "../admin/dist")));
+    app.use(express.static(path.join(__dirname, "admin/dist")));
 
     app.get("/{*any}", (req,res) => {
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "admin", "dist", "index.html"));
     });
 }
     console.log("Working");
