9fd5efcc67481e4a729fb8eb50881afdd86f1a51
Mase-mt <masamomathewos9@gmail.com>
2026-01-03 18:44:30 +0300

    admin dashboard tools setup

---
+++ admin/src/pages/LogInPage.jsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/clerk-react';
+import React from 'react'
+
+const LogInPage = () => {
+  return (
+    <div>
+      Log In Page
+      <SignIn/>
+    </div>
+  )
+}
+
+export default LogInPage;
---
+++ admin/src/pages/OrdersPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrderPage = () => {
+  return (
+    <div>
+      Orders page
+    </div>
+  )
+}
+
+export default OrderPage
