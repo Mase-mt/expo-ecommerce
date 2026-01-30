0b52bd5f0e1dd73d682270d4530f079c406e6d56
Mase-mt <masamomathewos9@gmail.com>
2026-01-30 20:37:44 +0300

    wishlist screen added

---
+++ mobile/app/(profile)/addresses.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SafeScreen from '@/components/SafeScreen'
+
+const AddressesScreen = () => {
+  return (
+    <SafeScreen>
+      <Text className="text-white">AddressesScreen</Text>
+    </SafeScreen>
+  )
+}
+
+export default AddressesScreen
